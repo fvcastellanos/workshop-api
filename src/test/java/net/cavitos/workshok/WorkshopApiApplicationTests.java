f1e1df6e7a0e65e0ed3100bca94793adaa5147e4
Francisco Castellanos <fvcastellanos@gmail.com>
2022-04-04 22:19:40 -0600

    initial commit

---
+++ src/test/java/net/cavitos/workshok/WorkshopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.cavitos.workshok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
